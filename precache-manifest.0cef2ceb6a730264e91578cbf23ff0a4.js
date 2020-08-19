2b087ae01c864c4265fa897508ac77b25c9ed139
Waqas Ayub <waqasayub897@gmail.com>
2020-08-19 15:38:50 +0500

    Updates

---
+++ precache-manifest.0cef2ceb6a730264e91578cbf23ff0a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30698bba43ade5320fdea453f84edbf9",
+    "url": "/robofriends2/index.html"
+  },
+  {
+    "revision": "7e7818df6a7c52bf6da4",
+    "url": "/robofriends2/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0a1923db43cc29ad4e11",
+    "url": "/robofriends2/static/css/main.ad3701a8.chunk.css"
+  },
+  {
+    "revision": "7e7818df6a7c52bf6da4",
+    "url": "/robofriends2/static/js/2.4b3f96cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends2/static/js/2.4b3f96cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a1923db43cc29ad4e11",
+    "url": "/robofriends2/static/js/main.70939de2.chunk.js"
+  },
+  {
+    "revision": "c210776d0b8ad6e4df13",
+    "url": "/robofriends2/static/js/runtime-main.cfa1dc89.js"
+  }
+]);
